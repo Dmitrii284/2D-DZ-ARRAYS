f9b1ce200a461180027fd1ee2b741065fda49f4e
Дмитрий <@ADMIN>
2023-02-18 22:36:36 +0500

    ДВУМЕРНЫЕ МАССИВЫ ДЗ

--- ДВУМЕРНЫЕ МАССИВЫ/arr.cpp
+++ ДВУМЕРНЫЕ МАССИВЫ/arr.cpp
@@ -6,12 +6,12 @@
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-	/*Дан двумерный массив с размерами 10 на 10,
-		заполненный случайными числами от 0 до 100.
-		Задача 1
-		Напишите программу, которая считает сумму
-		каждого отдельного ряда и выводит все найденные
-		суммы в консоль.*/
+	/* Given a two-dimensional array with dimensions 10 by 10,
+	delayed by random numbers from 0 to 100.
+	Task 1
+	Write a program that counts
+	each single line and outputs all found
+	amounts in the console.*/
 	const int size = 10;
 	int mx[size][size];
 	srand(time(NULL));
@@ -21,59 +21,37 @@ int main() {
 			mx[i][j] = rand() % 101; //[0..100]
 			std::cout << mx[i][j] << '\t';
 		}
-		std::cout << '\n';
-		
+		std::cout << '\n';		
 	}
-	/*int sumR = 0;
+	int sumR = 0;
 	for (int i = 0; i < size; i++) {
-		sumR = 0;
+			sumR = 0;
 		for (int j = 0; j < size; j++) {
 			sumR += mx[i][j];
 		}
 		std::cout << '\n' << sumR << '\n';// Тут добавляется к сумме каждого ряда . Надо вывести только одного.
-	}*/
-		
-
-
-	
+	}			
 	int k;
 	std::cout << "Enter line number col to sort -> ";
 	std::cin >> k;
-
-	
+	std::cout << "First way \n";
 			/*for (int i = size - 1; i >= 0; i--) {
 				for (int j = 0; j < i; j++) {
 					if (mx[k][j + 1] < mx[k][j])
 						std::swap(mx[k][j + 1], mx[k][j]);
-
-					
-
 				}
-				
-				
-			}*/
-			
-		/*
-				for (int j = 0; j < size; j++) {
-					
-					std::cout << mx[k][j] << '\t';
-				}
-				std::cout << '\n';
-				*/
-	std::sort(mx[k], mx[k] + size);
-		
-	//for (int i = 0; i < size; i++) {
+			}
+			for (int j = 0; j < size; j++){
+				std::cout << mx[k][j] << '\t';
+			}
+			std::cout << '\n';*/
+
+		std::sort(mx[k], mx[k] + size);		
+	//for (int i = 0; i < size; i++){
 		for (int j = 0; j < size; j++) {
-			
 			std::cout << mx[k][j] << '\t';
 		}
-		std::cout << '\n';
+			std::cout << '\n';
 	//}
-
-		
-
-
-
-
 	return 0;
 }
